2f391b70cb75b50cb06a5daff640dd036b3ed722
Brandon Hoge <bmhoge@gmail.com>
2022-02-20 18:22:31 -0600

    Code considers comments and compares sentiments

--- reddit_json.py
+++ reddit_json.py
@@ -1,4 +1,5 @@
 import praw
+from praw.models import MoreComments
 import time
 from datetime import datetime
 import csv
@@ -12,12 +13,20 @@ import json
 
 def get_movie_date(movie_name):
 
-        # Release date of movie ##################################################################
+        ######################################################### Release date of movie ##################################################################
         date_entry = '2021-10-22'
 
         input_year, input_month, input_day = map(int, date_entry.split('-'))
         release_date = datetime(input_year, input_month, input_day)
         return release_date
+
+def sentiment_check(sentence):
+    sid = SentimentIntensityAnalyzer()
+    ss = sid.polarity_scores(sentence)
+    if float(ss['pos']) > float([ss['neg']]):
+        return 'positive'
+    else:
+        return 'negative'
 
 def get_sentiment_averages(sentences):
     negative_total = 0
@@ -42,6 +51,45 @@ def get_sentiment_averages(sentences):
     }
     return averages_dict
 
+def write_to_json(stats_dictionary, filename):
+    json_string = json.dumps(stats_dictionary)
+    with open(filename, 'w') as input_dict:
+        json.dump(json_string, input_dict)
+
+def append_to_dict(dictionary, submission, comments):
+    dictionary['upvote_ratio'].append(submission.upvote_ratio)
+    dictionary['score'].append(submission.score)
+    dictionary['num_of_comments'].append(submission.num_comments)
+    dictionary['titles'].append(submission.title)
+    dictionary['comments'].append(comments)
+    return dictionary
+
+def total_sentiment_counts(dictionary):
+    total_pos = 0
+    total_neg = 0
+
+    for count, title in enumerate(dictionary['titles']):
+        avg_comment_sentiment = get_sentiment_averages(dictionary['comments'][count])
+        if float(avg_comment_sentiment['positive']) > float(avg_comment_sentiment['negative']):
+            overall_comment_sentiment = 'positive'
+        else:
+            overall_comment_sentiment = 'negative'
+
+        if title == 'positive' and overall_comment_sentiment == 'positive':
+            total_pos += 1
+        elif title == 'positive' and overall_comment_sentiment == 'negative':
+            total_neg += 1
+        elif title == 'negative' and overall_comment_sentiment == 'negative':
+            total_pos += 1
+        else:
+            total_neg += 1
+
+    pos_and_neg = {
+        'pos' : total_pos,
+        'neg' : total_neg
+    }
+    return pos_and_neg
+
 if __name__ == "__main__":
     movie_name = ''
 
@@ -52,68 +100,77 @@ if __name__ == "__main__":
         user_agent="pc:movie_genie:v1.0 (by u/movie_genie)",
         )
 
-    # Name of movie ##################################################################
+    ######################################### Name of movie ##################################################################
     movie_name = 'Dune'
     
     release_date = get_movie_date(movie_name)
-    before_upvotes = []
-    before_score = []
-    before_comments = []
-    before_titles = []
 
-    after_upvotes = []
-    after_score = []
-    after_comments = []
-    after_titles = []
+    before_dict = {
+        'upvote_ratio' : [],
+        'score' : [],
+        'num_of_comments' : [],
+        'titles' : [],
+        'comments' : []
+    }
 
+    after_dict = {
+        'upvote_ratio' : [],
+        'score' : [],
+        'num_of_comments' : [],
+        'titles' : [],
+        'comments' : []
+    }
 
+    # Loop through r/movies submissions searching by movie name
     for submission in reddit.subreddit('movies').search(query=movie_name):
 
         created_utc = (submission.created_utc)
         created_date = datetime.utcfromtimestamp(created_utc).strftime('%Y-%m-%d')
 
-        if str(created_date) < str(release_date):
-            before_upvotes.append(submission.upvote_ratio)
-            before_score.append(submission.score)
-            before_comments.append(submission.num_comments)
-            before_titles.append(submission.title)
+        # Loop through top level num_of_comments in submission (post)
+        comments = []
 
+        for top_level_comment in submission.comments:
+            if isinstance(top_level_comment, MoreComments):
+                continue
+            comments.append(top_level_comment.body)
+        if str(created_date) < str(release_date):
+            before_dict = append_to_dict(before_dict, submission, comments)
         else:
-            after_upvotes.append(submission.upvote_ratio)
-            after_score.append(submission.score)
-            after_comments.append(submission.num_comments)
-            after_titles.append(submission.title)
+            after_dict = append_to_dict(after_dict, submission, comments)
 
     # Before release date (averages)
-    before_ratio_total = round((sum(before_upvotes) / len(before_upvotes) * 100), 2)
-    before_score_total = round(sum(before_score) / len(before_score), 2)
-    before_comments_total = round(sum(before_comments) / len(before_comments), 2)
-    before_sentiment = get_sentiment_averages(before_titles)
+    before_ratio_total = round((sum(before_dict['upvote_ratio']) / len(before_dict['upvote_ratio']) * 100), 2)
+    before_score_total = round(sum(before_dict['score']) / len(before_dict['score']), 2)
+    before_comments_total = round(sum(before_dict['num_of_comments']) / len(before_dict['num_of_comments']), 2)
+    before_sentiment = get_sentiment_averages(before_dict['titles'])
+    before_sentiment_totals = total_sentiment_counts(before_dict)
 
     final_stats_before = {
         'Ratio' : before_ratio_total,
         'Posts' : before_score_total,
         'Comments' : before_comments_total,
-        'Sentiment' : before_sentiment
+        'Sentiment' : before_sentiment,
+        'Positive Posts' : before_sentiment_totals['pos'],
+        'Negative Posts' : before_sentiment_totals['neg']
     }
 
-    json_string = json.dumps(final_stats_before)
-    with open('before_stats.json', 'w') as input_dict:
-        json.dump(json_string, input_dict)
+    write_to_json(final_stats_before, 'before_stats.json')
 
     # After release date (averages)
-    after_ratio_total = round((sum(after_upvotes) / len(after_upvotes) * 100))
-    after_score_total = round(sum(after_score) / len(after_score), 2)
-    after_comments_total = round(sum(after_comments) / len(after_comments), 2)
-    after_sentiment = get_sentiment_averages(after_titles)
+    after_ratio_total = round((sum(after_dict['upvote_ratio']) / len(after_dict['upvote_ratio']) * 100))
+    after_score_total = round(sum(after_dict['score']) / len(after_dict['score']), 2)
+    after_comments_total = round(sum(after_dict['num_of_comments']) / len(after_dict['num_of_comments']), 2)
+    after_sentiment = get_sentiment_averages(after_dict['titles'])
+    after_sentiment_totals = total_sentiment_counts(after_dict)
 
     final_stats_after = {
         'Ratio' : after_ratio_total,
         'Posts' : after_score_total,
         'Comments' : after_comments_total,
-        'Sentiment' : after_sentiment
+        'Sentiment' : after_sentiment,
+        'Positive Posts' : after_sentiment_totals['pos'],
+        'Negative Posts' : after_sentiment_totals['neg']
     }
 
-    json_string = json.dumps(final_stats_after)
-    with open('after_stats.json', 'w') as input_dict:
-        json.dump(json_string, input_dict)
+    write_to_json(final_stats_after, 'after_stats.json')
